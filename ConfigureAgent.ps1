2840cb992a250af68172614e83af16d7c13ca79a
Tsuyoshi Ushio <ushio@simplearchitect.com>
2016-01-11 23:41:08 +0900

    misspel foreach and code refactoring

--- ConfigureAgent.ps1
+++ ConfigureAgent.ps1
@@ -17,14 +17,14 @@ if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall")){
 }
 if(!(Test-Path $env:ChocolateyInstall\lib\Psake*)) { cinst psake -y }
 if(!(Test-Path $env:ChocolateyInstall\lib\WindowsAzurePowershell*)) { cinst WindowsAzurePowershell -y }
-choco install visualstudio2015community -y --force
+if(!(Test-Path $env:ChocolateyInstall\lib\VisualStudio2015Community*)) { cinst visualstudio2015community -y --force}
 }
 
 function Install-Agent
 {
 $shell = New-Object -com shell.application
 $zip = $shell.NameSpace((Get-Location).Path + "\agent.zip")
-foraech($item in $zip.items())
+foreach($item in $zip.items())
 {
     $shell.Namespace((Get-Location).Path).copyhere($item)
 }
