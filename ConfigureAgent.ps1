030e2ed9f565ac8d90a1766f985d7af2fda67c3f
Adrian Cosmici <adrian.cosmici@aberdeen-asset.com>
2017-02-14 09:09:05 +0200

    Updated FileUri repo and added Java, Maven, Gradle, Ant, Gulp, NugetPackageManager and NodeJS to agent configuration

--- ConfigureAgent.ps1
+++ ConfigureAgent.ps1
@@ -16,6 +16,13 @@ if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall")){
     iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
 }
 if(!(Test-Path $env:ChocolateyInstall\lib\Psake*)) { cinst psake -y }
+if(!(Test-Path $env:ChocolateyInstall\lib\JavaSE*)) { cinst jdk8 -y }
+if(!(Test-Path $env:ChocolateyInstall\lib\Maven*)) { cinst maven -y }
+if(!(Test-Path $env:ChocolateyInstall\lib\Gradle*)) { cinst gradle -y }
+if(!(Test-Path $env:ChocolateyInstall\lib\Ant*)) { cinst ant -y }
+if(!(Test-Path $env:ChocolateyInstall\lib\Gulp-Cli*)) { cinst gulp-cli -y }
+if(!(Test-Path $env:ChocolateyInstall\lib\NugetPackageManager*)) { cinst nugetpackagemanager -y }
+if(!(Test-Path $env:ChocolateyInstall\lib\NodeJS*)) { cinst nodejs.install -y }
 if(!(Test-Path $env:ChocolateyInstall\lib\WindowsAzurePowershell*)) { cinst WindowsAzurePowershell -y }
 if(!(Test-Path $env:ChocolateyInstall\lib\VisualStudio2015Community*)) { cinst visualstudio2015community -y --force}
 }
